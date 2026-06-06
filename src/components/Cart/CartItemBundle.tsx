5ae9ab13754f79b5b831c59f092a4e510dbdf6b0
crimson_speedster <shvets.o@mavericks.agency>
2026-06-06 15:53:26 +0200

    fixes for bundles

--- src/components/Cart/CartItemBundle.tsx
+++ src/components/Cart/CartItemBundle.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import {Col, Row} from "react-bootstrap";
+import Image from "next/image";
+import FallbackImage from '@assets/images/fallback.png';
 import {CartItemBundleObject} from "@interfaces/entities/cart";
+import CartItemRemove from "@src/components/Cart/CartItemRemove";
+import React from "react";
 
 
 type Props = {
@@ -17,42 +21,76 @@ const CartItemBundle = (
         <Row className="g-0 border-bottom align-items-center py-3 border-bottom">
             <Col md={6}>
                 <div className="d-flex gap-3 align-items-start align-items-md-center">
-                    {/*<Image src={thumb1} alt="Thumb1Img" />*/}
+                    <div
+                        className=""
+                    >
+                        <Image
+                            src={FallbackImage}
+                            width={120}
+                            height={145}
+                            alt={item.entity.title}
+                            className="object-fit-cover object-center"
+                        />
+                    </div>
+
                     <div className="w-100">
                         <div className="px-2 pb-2">
-                            <h6 className="fs-16">Test</h6>
+                            <span
+                                className="fs-16"
+                            >
+                                {item.entity.title}
+                            </span>
+
+                            {
+                                item.product_variant &&
+                                <p className="text-muted fs-12">
+                                    {
+                                        item.product_variant.attribute_terms && item.product_variant.attribute_terms.length > 0 &&
+                                        item.product_variant.attribute_terms.map((attr, i) => (
+                                            <span
+                                                key={attr.id}
+                                            >
+                                        {
+                                            attr.title
+                                        }
+                                                {
+                                                    ' '
+                                                }
+                                    </span>
+                                        ))
+                                    }
+                                </p>
+                            }
 
                             <div className="mt-3">
-                                <svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <polyline points="3 6 5 6 21 6"></polyline>
-                                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                                    </path>
-                                    <line x1="10" y1="11" x2="10" y2="17"></line>
-                                    <line x1="14" y1="11" x2="14" y2="17"></line>
-                                </svg>
+                                <CartItemRemove
+                                    id={item.id}
+                                />
                             </div>
                         </div>
+
                         <div className="border-bottom border-top border-dotted p-2 d-md-none">
-                            <p className="text-muted m-0">$35.00</p>
+                            <p className="text-muted m-0">{item.price_formatted}</p>
                         </div>
 
-                        // QTY
-
                         <div className="border-top border-dotted p-2 d-md-none">
-                            <p className="m-0">$35.00</p>
+                            <p className="m-0">{item.total_formatted}</p>
+                        </div>
+
+                        <div className="d-md-none my-2">
+                            x {item.quantity}
                         </div>
                     </div>
                 </div>
             </Col>
 
             <Col md={6} className="justify-content-between d-none d-md-flex align-items-center">
-                <p className="text-muted">$35.00</p>
+                <p className="text-muted">{item.price_formatted}</p>
 
-                // QTY
+                <p className="text-muted">x {item.quantity}</p>
 
-                <p className="text-black text-end">$35.00</p>
+                <p className="text-black text-end">{item.total_formatted}</p>
             </Col>
-
         </Row>
     );
 }
--- src/components/MiniCart/MiniCartItemBundle.tsx
+++ src/components/MiniCart/MiniCartItemBundle.tsx
@@ -1,19 +1,79 @@
+import Image from "next/image";
 import {CartItemBundleObject} from "@interfaces/entities/cart";
+import FallbackImage from "@assets/images/fallback.png";
+import CartItemRemove from "@src/components/Cart/CartItemRemove";
 
 
 type Props = {
-    item: CartItemBundleObject;
+    item: CartItemBundleObject,
     handleShoppingClose: ()=>void,
-}
+};
 
 const MiniCartItemBundle = (
     {
-        item,
         handleShoppingClose,
+        item,
     }: Props
 ) => {
     return (
-        <p>Bundle</p>
+        <div className="p-20 border-top">
+            <div className="row">
+                <div className="col-5">
+                    <div>
+                        <Image
+                            src={FallbackImage}
+                            width={110}
+                            height={137}
+                            alt={item.entity.title}
+                            className="object-fit-cover object-center"
+                        />
+                    </div>
+                </div>
+
+                <div className="col-7">
+                    <h6 className="mb-1">
+                        <span
+                            className="product-title"
+                        >
+                            {item.entity.title}
+                        </span>
+                    </h6>
+
+                    {
+                        item.product_variant &&
+                        <p className="text-muted fs-12">
+                            {
+                                item.product_variant.attribute_terms && item.product_variant.attribute_terms.length > 0 &&
+                                item.product_variant.attribute_terms.map((attr, i) => (
+                                    <span
+                                        key={attr.id}
+                                    >
+                                        {
+                                            attr.title
+                                        }
+                                        {
+                                            ' '
+                                        }
+                                    </span>
+                                ))
+                            }
+                        </p>
+                    }
+
+                    <p className="fs-14 text-muted d-flex align-items-center gap-2">
+                        <span>{item.price_formatted}</span>
+                    </p>
+
+                    <p className="text-muted fs-14">x {item.quantity}</p>
+
+                    <div className="d-flex align-items-center gap-3 mt-2">
+                        <CartItemRemove
+                            id={item.id}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
